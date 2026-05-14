450c34c7a2b182da4a59ffbd9377592277c04ca6
jeongho <jeongho.sim@samsung.com>
2026-05-14 11:27:51 +0900

    19-deploy: step01.3 - middleware re-export 구문 명시적으로 import 후 export로 변경(빌드 문제 해결)

--- middleware.ts
+++ middleware.ts
@@ -1,5 +1,7 @@
 // middleware.ts
-export { default } from "next-auth/middleware";
+import { default as middleware } from "next-auth/middleware";
+
+export default middleware;
 
 export const config = {
   matcher: ["/write", "/dashboard/:path*"], // ⭐ 보호할 경로
